d3ce1ddf492331c1bdd7ed91589947f510b47a44
IsraelDXPP <nydilupi75@hotmail.com>
2026-06-20 13:58:16 -0700

    build(ios): add C++ stream polyfills for iOS 9 and dynamically lookup ___gxx_personality_sj0

---
+++ src/ios_cxx_polyfills.cpp
@@ -0,0 +1,20 @@
+#include <istream>
+#include <ostream>
+#include <fstream>
+#include <string>
+
+// Xcode 15 C++ headers assume these are in libc++.dylib, but iOS 9/10 libc++.dylib doesn't have them.
+// By explicitly instantiating them here, we force the compiler to generate the code in our binary.
+
+_LIBCPP_BEGIN_NAMESPACE_STD
+
+template basic_istream<char, char_traits<char>>& basic_istream<char, char_traits<char>>::read(char_type*, streamsize);
+template basic_ostream<char, char_traits<char>>& basic_ostream<char, char_traits<char>>::write(const char_type*, streamsize);
+
+template streamsize basic_streambuf<char, char_traits<char>>::xsgetn(char_type*, streamsize);
+template streamsize basic_streambuf<char, char_traits<char>>::xsputn(const char_type*, streamsize);
+
+template class basic_ifstream<char, char_traits<char>>;
+template class basic_ofstream<char, char_traits<char>>;
+
+_LIBCPP_END_NAMESPACE_STD
